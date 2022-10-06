bcb6c8b66c8cd29a815ba760c785c0a2405c4f9d
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2022-10-06 21:50:16 +0530

    Assignmennt 1 of LU Bootcamp

---
+++ a1.py
@@ -0,0 +1,7 @@
+# Accepting an input from the user
+num = int(input("Enter any number: "))
+print("")
+if num%2==0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number")
