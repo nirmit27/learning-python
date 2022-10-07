276fdc4f762e4ed251497bebe6788855e8f8a685
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2022-10-07 18:24:34 +0530

    HW

--- P2.py
+++ P2.py
@@ -1,8 +1,10 @@
-a = int(input('Enter the number:	'))
-if a>1:
-	for i in range(2,int(a**0.5)+1) :
-		if (a%i==0) :
-			print('Number {} is not prime'.format(a))
-			break
-	else :
-		print('Number {} is prime'.format(a))
+a = int(input('Enter the number:\t'))
+def primeOrNot(a):
+	if a>1:
+		for i in range(2,int(a**0.5)+1) :
+			if (a%i==0) :
+				print('{} is not prime.'.format(a))
+				break
+		else :
+			print('{} is prime.'.format(a))
+primeOrNot(a)
--- a1.py
+++ a1.py
@@ -1,7 +1,10 @@
 # Accepting an input from the user
 num = int(input("Enter any number: "))
-print("")
-if num%2==0:
-    print(f"{num} is an even number.")
-else:
-    print(f"{num} is an odd number")
+
+def OddOrEven(num):
+    if num%2==0:
+        print(f"{num} is an even number.")
+    else:
+        print(f"{num} is an odd number")
+
+OddOrEven(num)
