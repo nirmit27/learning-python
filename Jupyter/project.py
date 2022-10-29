d3f887d7b46deee5126fd980fa4504aa6a7a4ecf
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2022-10-29 22:32:08 +0530

    Added a prompt and changed the URl

--- Jupyter/project.py
+++ Jupyter/project.py
@@ -1,11 +1,13 @@
+# %% [markdown]
+# Always Remember To Take A Break Dude!
+
 # %%
 import time
 import webbrowser
 
-breaks = 3
+breaks = 2
+n = int(input("Enter the number of hours in between two successive breaks: "))
 
 for i in range(breaks):
-    time.sleep(1)
-    webbrowser.open('https://animixplay.to/',new=0)
-
-
+    time.sleep(n*60*60)
+    webbrowser.open('https://www.youtube.com/',new=2)
