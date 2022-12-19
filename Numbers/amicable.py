25237640067c61057d6962cf6b800bb499b2ca00
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2022-12-19 19:17:35 +0530

    pep8 formatter

--- Numbers/amicable.py
+++ Numbers/amicable.py
@@ -3,14 +3,16 @@
 a = int(input("\n Enter the first number : "))
 b = int(input("\n Enter the second number : "))
 
+
 def amicable(x):
-    sum=0
-    for i in range(1,x):
-        if x%i==0:
-            sum+=i
+    sum = 0
+    for i in range(1, x):
+        if x % i == 0:
+            sum += i
     return sum
 
-if (amicable(a)==b and amicable(b)==a):
+
+if (amicable(a) == b and amicable(b) == a):
     print(f"\n {a} and {b} are amicable numbers.\n")
 else:
     print(f"\n {a} and {b} are not amicable numbers.\n")
