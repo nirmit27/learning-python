2581d77c3fb4578cb62a1f6a230789f7b8dfa964
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2022-12-21 07:11:58 +0530

    menu-driven!

--- Patterns/starpy.py
+++ Patterns/starpy.py
@@ -1,15 +1,103 @@
-"""        *
-          * *
-         * * *
-        * * * *
-       * * * * *        """
+# Star Patterns
 
-rows = int(input("Enter the number of rows: "))
-space=rows
-for i in range(0,rows):
-    for sp in range(space):
-        print(end=' ')
-    for j in range(0,i+1):
-        print('*',end=' ')
-    print()
-    space-=1
+menu = "STAR PATTERN PROGRAM"
+print(f"\n{menu.center(32)}")
+print("-------------------------------")
+opt = 1
+
+while opt:
+  opt = list(input(" Enter the rows and type : ").split(' '))
+  row = int(opt[0])
+  ch = int(opt[1])
+
+  print("\n Here's your pattern ...\n")
+
+  if ch == 1:
+    #           *
+    #         * *
+    #       * * *
+    #     * * * *
+    #   * * * * *
+    for i in range(row):
+      sp = row
+      j = 0
+      print("\t\t", end='')
+      while (1):
+        if sp > i:
+          print("  ", end='')  # 2 spaces
+          sp -= 1
+          continue
+        elif j <= i:
+          print("* ", end='')
+          j += 1
+          continue
+        else:
+          break
+      print()
+
+  elif ch == 2:
+    #   *
+    #   * *
+    #   * * *
+    #   * * * *
+    #   * * * * *
+    for i in range(row):
+      j = 0
+      print("\t\t", end='')
+      while (1):
+        if j <= i:
+          print("* ", end='')
+          j += 1
+          continue
+        else:
+          break
+      print()
+
+  elif ch == 3:
+    #       *
+    #      * *
+    #     * * *
+    #    * * * *
+    #   * * * * *
+    for i in range(row):
+      sp = row
+      j = 0
+      print("\t\t", end='')
+      while (1):
+        if sp > i:
+          print(" ", end='')  # 1 space
+          sp -= 1
+          continue
+        elif j <= i:
+          print("* ", end='')
+          j += 1
+          continue
+        else:
+          break
+      print()
+
+  elif ch == 4:
+    #   * * * * *
+    #    * * * *
+    #     * * *
+    #      * *
+    #       *
+    for i in range(row, 0, -1):
+      sp = row - i
+      j = 0
+      print("\t\t", end='')
+      while (1):
+        if sp > 0:
+          print(" ", end='')  # 1 space
+          sp -= 1
+          continue
+        elif j < i:
+          print("* ", end='')
+          j += 1
+          continue
+        else:
+          break
+      print()
+  
+  opt = int(input("\n Want more? (0/1)\t"))
+  print("-------------------------------")
