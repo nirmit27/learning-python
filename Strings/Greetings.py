2f5d67c36cb6444d45be4e78203cc84037afa067
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2022-12-23 19:52:13 +0530

    added a sleeping timer

--- Strings/Greetings.py
+++ Strings/Greetings.py
@@ -3,9 +3,15 @@ import time as t
 
 hour = int(t.strftime('%H'))
 
-if hour in range(0,13):
-  print("\n Good Morning, Sir!")
-elif hour in range(13,19):
-  print("\n Good Afternoon, Sir!")
-elif hour in range(19,21):
-  print("\n Good Evening, Sir!")
+def greet(hour):
+    if hour in range(0, 13):
+        print("\n Good Morning, Sir!\n")
+    elif hour in range(13, 19):
+        print("\n Good Afternoon, Sir!\n")
+    elif hour in range(19, 21):
+        print("\n Good Evening, Sir!\n")
+    elif hour in range(21, 0):
+        print("\n Good Night, Sir!\n")
+
+t.sleep(5)
+greet(hour)
