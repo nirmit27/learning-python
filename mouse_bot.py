74715364328a683be596c655c7c1475a1b760e30
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2023-04-12 23:04:12 +0530

    Mouse Bot program

---
+++ mouse_bot.py
@@ -0,0 +1,28 @@
+# You'll never be AFK with this script running in the background ...
+
+import time as t
+import random as r
+import pyautogui as pag
+
+
+def mouse_bot(s):
+
+    while True:
+
+        # Co-ordinates within which the mouse moves on the screen ...
+        x, y = r.randint(600, 700), r.randint(200, 600)
+
+        # Invoking the module's function ...
+        pag.moveTo(x, y, s)
+
+        # Interval between successive mouse events ...
+        t.sleep(2)
+
+
+# Driver code
+if __name__ == "__main__":
+
+    speed = float(input(
+        "\n Enter the mouse speed : \n\n (0.0 => instantaneous movement 😮)\n (0.5 => a little slower ... 😗) \n (1.0 => quiet slow to be honest 😐)\n\n Your choice >> "))
+    print("\n You can do something else now. 😁\n\n (Press Ctrl+C to stop the bot.)\n")
+    mouse_bot(speed)
