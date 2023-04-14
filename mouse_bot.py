a2b4fb1c7baed3420371098be8690d5ab7b67303
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2023-04-14 21:03:45 +0530

    Update mouse_bot.py
    Two changes 😁
    Changed the o/p menu.
    Added an exception handling block to display a meaningful message on the CLI.

--- mouse_bot.py
+++ mouse_bot.py
@@ -22,7 +22,13 @@ def mouse_bot(s):
 # Driver code
 if __name__ == "__main__":
 
-    speed = float(input(
-        "\n Enter the mouse speed : \n\n (0.0 => instantaneous movement 😮)\n (0.5 => a little slower ... 😗) \n (1.0 => quiet slow to be honest 😐)\n\n Your choice >> "))
-    print("\n You can do something else now. 😁\n\n (Press Ctrl+C to stop the bot.)\n")
-    mouse_bot(speed)
+    menu = "M O U S E  B O T".center(30, ' ')
+
+    try:
+        speed = float(input(
+            f"\n{menu}\n\n Enter the mouse speed : \n\n (0.0 => instantaneous movement 😮)\n (0.5 => a little  slower ... 😗) \n (1.0 => quiet slow to be honest 😐)\n\n Your choice >> "))
+        print("\n You can go AFK now. 😁\n\n\n (Press Ctrl+C to stop the bot.)")
+        mouse_bot(speed)
+#
+    except KeyboardInterrupt:
+        print("\n\n The bot has been stopped! 😶")
