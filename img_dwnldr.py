d8d85db89a44fd9609cbb445679a2687f15579cd
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2024-07-25 15:50:12 +0530

    Automated image download
    Using the icrawler and bing-image-downloader libraries, automated downloads of images into local machine

---
+++ img_dwnldr.py
@@ -0,0 +1,27 @@
+""" For the automated downloading of images. """
+from bing_image_downloader import downloader
+from icrawler.builtin import GoogleImageCrawler
+
+
+def bing_downloader(query, op_dir, limit):
+    downloader.download(query=query, limit=limit, output_dir=op_dir,
+                        adult_filter_off=True, force_replace=False, timeout=60)
+
+
+def google_downloader(keyword, op_dir, limit):
+    google_crawler = GoogleImageCrawler(storage={"root_dir": op_dir})
+    google_crawler.crawl(keyword=keyword, max_num=limit)
+
+
+def main():
+    limit: int = int(input("Enter the limit (preferably don't exceed 100) : "))
+    query: str = input(
+        "\nEnter the keywords seperated by a space : ") or "white eggs"
+    op_dir: str = input(
+        "\nEnter the file download path : ") or r"data\white_eggs"
+    google_downloader(query, op_dir, limit)
+    # bing_downloader(query, op_dir, limit)
+
+
+if __name__ == "__main__":
+    main()
