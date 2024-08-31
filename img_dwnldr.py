9ae37ba057f0b21cf5370946037d73ad59019905
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2024-08-31 19:00:27 +0530

    Update img_dwnldr.py

--- img_dwnldr.py
+++ img_dwnldr.py
@@ -3,17 +3,17 @@ from bing_image_downloader import downloader
 from icrawler.builtin import GoogleImageCrawler
 
 
-def bing_downloader(query, op_dir, limit):
+def bing_downloader(query, op_dir, limit) -> None:
     downloader.download(query=query, limit=limit, output_dir=op_dir,
                         adult_filter_off=True, force_replace=False, timeout=60)
 
 
-def google_downloader(keyword, op_dir, limit):
+def google_downloader(keyword, op_dir, limit) -> None:
     google_crawler = GoogleImageCrawler(storage={"root_dir": op_dir})
     google_crawler.crawl(keyword=keyword, max_num=limit)
 
 
-def main():
+def main() -> None:
     limit: int = int(input("Enter the limit (preferably don't exceed 100) : ")) or 10
     query: str = input(
         "\nEnter the keywords seperated by a space : ") or "white eggs"
